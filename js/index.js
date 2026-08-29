9161dc2240fd4865daf32d34fd0d557031b8931e
Rafinbolis <rfdepaulas27@gmail.com>
2026-08-29 02:18:19 -0300

    começo do js no projeto

--- js/index.js
+++ js/index.js
@@ -1,10 +1,25 @@
 // consultar uma cidade pelo nome na api e verificar sua temperatura, tema do exercicio
 
-
-
 function acao (evento){
     let entrada= document.querySelector("#cidade").value;
+
     if (evento.key=="Enter"){
-        console.log(entrada)
+        cidade = pegarCidade(entrada);
+        console.log(entrada);
     }
+    return(cidade)
+}
+
+async function pegarCidade(nome){
+    let nomecidade = await fetch(
+        `https://brasilapi.com.br/api/cptec/v1/cidade/${nome}`
+    ); 
+    if (nomecidade.ok){
+        console.log("deu certo");
+        console.log(nomecidade.value)
+        return(nomecidade);
+    }
+    else{
+        console.log("deu red");
+    }  
 }
