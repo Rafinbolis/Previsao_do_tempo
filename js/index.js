6dd28c17032e76fa5d7188842619066900a0857f
Rafinbolis <rfdepaulas27@gmail.com>
2026-08-31 16:53:39 -0300

    reiniciando o js

--- js/index.js
+++ js/index.js
@@ -1,26 +1,13 @@
 // consultar uma cidade pelo nome na api e verificar sua temperatura, tema do exercicio
-let campoCidade = document.querySelector("#cidade");
+let campoCidade = document.querySelector("#cidade")
 
-campoCidade.addEventListener("keydown", async function () {
-
-    if(evento.key === "Enter"){
-        let entrada = campoCidade.value;
-        let cidade = await pegarCidade(entrada);
-        console.log(cidade)
+async function pegarCidade(event){
+    if(event.key==="Enter"){
+        let cidade = await fatch(
+            ``
+        )
+        console.log("deu certo")
+        console.log(campoCidade.value)
     }
-})
 
-
-async function pegarCidade(nome){
-    let dadoscidade = await fetch(
-        `https://brasilapi.com.br/api/cptec/v1/cidade/${nome}`
-    ); 
-    if (nomecidade.ok){
-        console.log("deu certo");
-        console.log(dadoscidade.value)
-        return(dadoscidade);
-    }
-    else{
-        console.log("deu red");
-    }  
 }
