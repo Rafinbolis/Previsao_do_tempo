b23cf027a721e47ccc723a4ece48e98b037a28b8
Rafinbolis <rfdepaulas27@gmail.com>
2026-09-01 20:22:19 -0300

    metade do js feito

--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
 // consultar uma cidade pelo nome na api e verificar sua temperatura, tema do exercicio
-let campoCidade = document.querySelector("#cidade")
+let campoCidade = document.querySelector("#cidade");
 
 async function pegarCidade(event){
     if(event.key==="Enter"){
-        let cidade = await fatch(
-            ``
-        )
-        console.log("deu certo")
-        console.log(campoCidade.value)
-    }
+
+        // buscando na api as informações da cidade escrita dentro do imput
+        let cidade = await fetch(
+            `https://brasilapi.com.br/api/cptec/v1/cidade/${campoCidade.value}`
+        );
+           
+        //tranforma os dados obtidos da api em um array e pega só o valor do id para inserir na requisição da api de tempo
+        let dadosCidade =  await cidade.json();
+        let id = dadosCidade.map(dadosCidade => dadosCidade.id);
+
+        //atraves da api com o id pega as informações sobre o tempo na cidade solicitada 
+        let tempo = await fetch(
+            `https://brasilapi.com.br/api/cptec/v1/clima/previsao/${id}`
+        );
+
+        let dadosTempo = await tempo.json();
+        let clima = dadosTempo.clima[0];
+
+        console.log(dadosCidade[0]);
+        console.log(id);
+        console.log(dadosTempo)
+        console.log(clima)
+    }   
 
 }
